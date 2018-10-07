db6085fd2b8a7f1f70ae87e719d3332f6a19cdf1
Wolfy-J <wolfy.jd@gmail.com>
2018-10-07 14:46:42 +0300

    more tests

--- src/Config/StemplerConfig.php
+++ src/Config/StemplerConfig.php
@@ -8,7 +8,9 @@
 
 namespace Spiral\Stempler\Config;
 
+use Spiral\Core\Container\Autowire;
 use Spiral\Core\InjectableConfig;
+use Spiral\Stempler\Exception\ConfigException;
 
 class StemplerConfig extends InjectableConfig
 {
@@ -19,4 +21,49 @@ class StemplerConfig extends InjectableConfig
         'processors'     => [],
         'postProcessors' => []
     ];
+
+    /**
+     * @return Autowire[]
+     */
+    public function getProcessors(): array
+    {
+        $processors = [];
+        foreach ($this->config['processors'] as $processor) {
+            $processors[] = $this->wire($processor);
+        }
+
+        return $processors;
+    }
+
+    /**
+     * @return Autowire[]
+     */
+    public function getPostProcessors(): array
+    {
+        $processors = [];
+        foreach ($this->config['postProcessors'] as $processor) {
+            $processors[] = $this->wire($processor);
+        }
+
+        return $processors;
+    }
+
+    /**
+     * @param mixed $item
+     * @return Autowire
+     *
+     * @throws ConfigException
+     */
+    public function wire($item): Autowire
+    {
+        if ($item instanceof Autowire) {
+            return $item;
+        }
+
+        if (is_string($item)) {
+            return new Autowire($item);
+        }
+
+        throw new ConfigException("Invalid class reference in view config.");
+    }
 }
